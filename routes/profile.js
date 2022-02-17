2323ce40c37a603d5dd6e90238eac569890bccb3
Cihaan <guide7169@gmail.com>
2022-02-17 16:50:19 +0100

    change password post

--- routes/profile.js
+++ routes/profile.js
@@ -8,21 +8,24 @@ require("dotenv");
 
 router.use(cors());
 
-router.post("/info/:id",authenticateToken, async (req, res) => {
+router.post("/info/:id", authenticateToken, async (req, res) => {
   let body = req.body;
   const id = parseInt(req.params.id);
 
   try {
     const newUser = await prisma.person.update({
-        where: {
-            id: id
-        },
+      where: {
+        id: id,
+      },
       data: {
         username: body.username,
-        email: body.email
+        email: body.email,
       },
     });
-    res.status(200).json(newUser);
+    res.status(200).json({
+      title: "Notification",
+      message: "Your informations have been updated successfully",
+    });
   } catch (e) {
     res.status(400).json({
       type: "Oops",
@@ -31,8 +34,41 @@ router.post("/info/:id",authenticateToken, async (req, res) => {
   }
 });
 
-router.post("/pwd", async (req, res) => {
-    //update password here
+router.post("/pwd/:id", authenticateToken, async (req, res) => {
+  let body = req.body;
+  const id = parseInt(req.params.id);
+
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashed = await bcrypt.hash(body.password, salt);
+
+    if (body.password !== body.cpassword) {
+      res.status(400).json({
+        type: "Oops",
+        message: "The provided passwords don't match",
+      });
+    }
+
+    const newPassword = await prisma.person.update({
+      where: {
+        id: id,
+      },
+      data: {
+        password: hashed,
+      },
+    });
+
+    res.status(200).json({
+      title: "Notification",
+      message: "Your informations have been updated successfully",
+    });
+  } catch (error) {
+    throw error
+    // res.status(400).json({
+    //   type: "Oops",
+    //   message: "The provided passwords don't match",
+    // });
+  }
 });
 
 module.exports = router;
